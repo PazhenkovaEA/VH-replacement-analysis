94985596e24927a93b1032cdcd7ec7e1a56b5afe
PazhenkovaEA <36327041+PazhenkovaEA@users.noreply.github.com>
2018-03-28 19:12:03 +0300

    Update genjunc.py

--- IG/genjunc.py
+++ IG/genjunc.py
@@ -3,7 +3,7 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import pandas as pd
 import os, random, sys
-
+#If user didn't specify parameters "stop-codons" and "framework shifts", then generate only productive sequences.
 try:
     fr = int(sys.argv[4])
 except:
@@ -12,6 +12,8 @@ try:
     stop = int(sys.argv[3])
 except:
     stop = 1
+
+ #Import databases with germline sequences.   
 handle = open('./V.fasta')
 records = list(SeqIO.parse(handle, "fasta"))
 V = [str(records[x].seq) for x in range(len(records)) ]
@@ -30,7 +32,7 @@ if not os.path.exists('Annotations'):
     os.makedirs('Annotations')
 
 nucleotides = 'agtc'
-
+#Functions for 3' and 5' palindroms. 
 def pal_3 (sequence):
     pal = sequence[-random.randint(1, 3):]
     my_seq = str(Seq(pal).reverse_complement())
@@ -40,19 +42,19 @@ def pal_5 (sequence):
     pal = sequence[:random.randint(1, 3)]
     my_seq = str(Seq(pal).reverse_complement())
     return my_seq
-
+#Function for N-nucleotides
 def n_region ():
     n_reg = ''
     for n in range(random.randint(5, 20)):
         n_reg += random.choice(nucleotides)
     return n_reg
-for n in range (int(sys.argv[1])):
+for n in range (int(sys.argv[1])): #Generate n sequences
     V1, D1, J1  = random.choice(V), random.choice(D), random.choice(J)
     p1, n1, p2, p3, n2, p4 = pal_3(V1), n_region(), pal_5(D1), pal_3(D1), n_region(), pal_5(J1)
     V_dom = V1 + p1 + n1 + p2 + D1 + p3 + n2 + p4 + J1
     len_n1 = len(n1)
     if fr == 1:
-        if len(V_dom) % 3 == 1: #лечим сдвиг рамки считывания
+        if len(V_dom) % 3 == 1: #Avoid frameshifts
             V_dom = V_dom.replace(V_dom[len(V1) + len(p1) + 1:len(V1) + len(p1) + len(n1)],
                                   V_dom[len(V1) + len(p1) + 1:len(V1) + len(p1) + len(n1)] + random.choice(nucleotides) + random.choice(nucleotides))
             len_n1 +=2
@@ -68,10 +70,10 @@ for n in range (int(sys.argv[1])):
                   "N2-REGION": [len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) +1, len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2)],
                   "P-REGION J 5'": [len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2) + 1, len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2) + len(p4)],
                   "J-REGION": [len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2) + len(p4), len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2) + len(p4) + len(J1)]}
-    df = pd.DataFrame.from_dict(annotation, orient = 'index') #это всё ради того, чтобы знать исходную разметку.
-    df.to_csv("./Annotations/" + str(n) + "ann.csv", encoding='utf-8', index=True)
+    df = pd.DataFrame.from_dict(annotation, orient = 'index') 
+    df.to_csv("./Annotations/" + str(n) + "ann.csv", encoding='utf-8', index=True) #Generate .csv files with sequence annotation.
     if stop == 1:
-        V_dom = V_dom.replace("tag", "tac") #тк слишком часто образуются стоп-кодоны, то мы грубо избавимся от них.
+        V_dom = V_dom.replace("tag", "tac") #Replace stop-codons
         V_dom = V_dom.replace("taa", "tac")
         V_dom = V_dom.replace("tga", "tgc")
     var = SeqRecord(Seq(V_dom), id = str(n) + "seq")
