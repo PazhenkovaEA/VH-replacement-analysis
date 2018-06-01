f2d17e6b51692dab84745add2f4c68b16681447b
PazhenkovaEA <36327041+PazhenkovaEA@users.noreply.github.com>
2018-06-01 17:44:45 +0400

    generator_v.3

--- IG/genejunc.py
+++ IG/genejunc.py
@@ -1,12 +1,18 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
 from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 import pandas as pd
-import os, random, sys
+import random, sys
 import numpy as np
-import matplotlib.pyplot as plt
 
-try:
+#Set the frameshifts and stop-codons presence treatment (default = true)
+try: 
     fr = int(sys.argv[4])
 except:
     fr = 1
@@ -14,7 +20,8 @@ try:
     stop = int(sys.argv[3])
 except:
     stop = 1
-handle = open('./V.fasta')
+#Open fasta with V, D and J genes
+handle = open('./V.fasta') 
 records = list(SeqIO.parse(handle, "fasta"))
 V = [str(records[x].seq) for x in range(len(records)) ]
 handle.close()
@@ -28,53 +35,68 @@ handle = open('./J.fas')
 records = list(SeqIO.parse(handle, "fasta"))
 J = [str(records[x].seq) for x in range(len(records)) ]
 handle.close()
-if not os.path.exists('Annotations'):
-    os.makedirs('Annotations')
 
 nucleotides = 'agtc'
+leng = pd.DataFrame(columns =["SeqID", "N1 length"]) #dataframe for lengths
+annotation = pd.DataFrame(columns = ["SeqID", "V-REGION", "P-REGION V 3'", "N1-REGION","P-REGION D5'", "D-REGION", "P-REGION D3'",
+                  "N2-REGION", "P-REGION J 5'","J-REGION"]) #dataframe for annotation
+#Set initial parameters of the N1-zone formation
+pp = 0.5 #p-nucleotides
+pn = 0.1 #n-nucleotides
+pe = 0.9 #exonuclease activity
 
-def pal_3 (sequence):
-    pal = sequence[-random.randint(1, 3):]
+
+def pal_3 (sequence): #3' palindrom formation
+    pal = sequence[-np.random.geometric(pp):]
     my_seq = str(Seq(pal).reverse_complement())
     return my_seq
 
-def pal_5 (sequence):
-    pal = sequence[:random.randint(1, 3)]
+def pal_5 (sequence): #5' palindrom formation
+    pal = sequence[:np.random.geometric(pp)]
     my_seq = str(Seq(pal).reverse_complement())
     return my_seq
 
-def n_region ():
+def n_region (): #n region formation
     n_reg = ''
-    for n in range(random.randint(5, 20)):
+    for n in range(np.random.geometric(pn)):
         n_reg += random.choice(nucleotides)
     return n_reg
 
-for n in range (int(sys.argv[1])):
+def exonuclease(seq): #exonucleases
+    new_seq = ''
+    for n in range(len(seq)):
+        if np.random.binomial(n = 1, p = pe) == 1:
+            new_seq += seq[n]
+        else:
+            continue
+    return(new_seq)
+            
+        
+for n in range (int(sys.argv[1])): #generation of antibodies
     V1, D1, J1  = random.choice(V), random.choice(D), random.choice(J)
-    p1, n1, p2, p3, n2, p4 = pal_3(V1), n_region(), pal_5(D1), pal_3(D1), n_region(), pal_5(J1)
+    p1, n1, p2, p3, n2, p4 = exonuclease(pal_3(V1)), exonuclease(n_region()), exonuclease(pal_5(D1)), exonuclease(pal_3(D1)), exonuclease(n_region()), exonuclease(pal_5(J1))
     V_dom = V1 + p1 + n1 + p2 + D1 + p3 + n2 + p4 + J1
-    len_n1 = len(n1)
+    len_n2 = len(n2)
     if fr == 1:
-        if len(V_dom) % 3 == 1: #лечим сдвиг рамки считывания
-            V_dom = V_dom.replace(V_dom[len(V1) + len(p1) + 1:len(V1) + len(p1) + len(n1)],
-                                  V_dom[len(V1) + len(p1) + 1:len(V1) + len(p1) + len(n1)] + random.choice(nucleotides) + random.choice(nucleotides))
-            len_n1 +=2
+        if len(V_dom) % 3 == 1: #frameshift treatment
+            V_dom = V_dom.replace(V_dom[len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + 1:len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + len_n2],
+                                  V_dom[len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + 1:len(V1) + len(p1) + len(n1) + + len(p2) + len(D1) + len(p3) + len_n2] + random.choice(nucleotides) + random.choice(nucleotides))
+            len_n2 +=2
         elif len(V_dom) % 3 == 2:
-            V_dom = V_dom.replace(V_dom[len(V1) + len(p1) + 1:len(V1) + len(p1) + len(n1)],
-                                  V_dom[len(V1) + len(p1) + 1:len(V1) + len(p1) + len(n1)] + random.choice(nucleotides))
-            len_n1 +=1
+            V_dom = V_dom.replace(V_dom[len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + 1:len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + len_n2],
+                                  V_dom[len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + 1:len(V1) + len(p1) + len(n1) + + len(p2) + len(D1) + len(p3) + len_n2] + random.choice(nucleotides))
+            len_n2 +=1
         else:
             pass
-    annotation = {"V-REGION" : [1, len(V1)], "P-REGION V 3'" : [len(V1)+1,len(V1) + len(p1)], "N1-REGION" : [len(V1) + len(p1) +1 , len(V1) + len(p1) + len_n1],
-                  "P-REGION D5'": [len(V1) + len(p1) + len_n1 + 1, len(V1) + len(p1) + len_n1 + len(p2)], "D-REGION": [len(V1) + len(p1) + len_n1 + len(p2) + 1, len(V1) + len(p1) + len_n1 + len(p2) + len(D1)],
-                  "P-REGION D3'" : [len(V1) + len(p1) + len_n1 + len(p2) + len(D1) +1, len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3)],
-                  "N2-REGION": [len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) +1, len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2)],
-                  "P-REGION J 5'": [len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2) + 1, len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2) + len(p4)],
-                  "J-REGION": [len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2) + len(p4), len(V1) + len(p1) + len_n1 + len(p2) + len(D1) + len(p3) + len(n2) + len(p4) + len(J1)]}
-    df = pd.DataFrame.from_dict(annotation) 
-    df.to_csv("./Annotations/" + str(n) + "ann.csv", encoding='utf-8', index=True)
+    annotation.append(pd.DataFrame([["seq" + str(n), len(V1),len(V1) + len(p1), len(V1) + len(p1) + len(n1),
+                                    len(V1) + len(p1) + len(n1) + len(p2), len(V1) + len(p1) + len(n1) + len(p2) + len(D1), len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3),
+                                    len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + len_n2, len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + len_n2 + len(p4), len(V1) + len(p1) + len(n1) + len(p2) + len(D1) + len(p3) + len_n2 + len(p4) + len(J1)]], 
+                                   columns = ["SeqID", "V-REGION", "P-REGION V 3'", "N1-REGION","P-REGION D5'", "D-REGION", "P-REGION D3'","N2-REGION", "P-REGION J 5'","J-REGION"]))
+
+                
+    leng.append(pd.DataFrame([["seq"+str(n), len(p1) + len(n1) + len(p2)]], columns =["SeqID", "N1 length"]))
     if stop == 1:
-        V_dom = V_dom.replace("tag", "tac") 
+        V_dom = V_dom.replace("tag", "tac")
         V_dom = V_dom.replace("taa", "tac")
         V_dom = V_dom.replace("tga", "tgc")
     var = SeqRecord(Seq(V_dom), id = str(n) + "seq")
@@ -82,4 +104,7 @@ for n in range (int(sys.argv[1])):
     SeqIO.write(var, output_handle, "fasta"),
     output_handle.close()
     n+=1
+ 
+annotation.to_csv("./Annotations_" +  str(sys.argv[2]) + ".csv", encoding='utf-8', index=True)
+leng.to_csv("./length_N1_" + str(sys.argv[2]) +".csv", encoding='utf-8', index=True) 
 
